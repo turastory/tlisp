8882dc7568cfe589d590cc426d3ed63ccb63c8f4
turastory <soldier4443@gmail.com>
2021-09-07 00:44:40 +0900

    Add lval struct

--- parsing.c
+++ parsing.c
@@ -29,6 +29,72 @@ void add_history(char *unused) {}
 #include "mpc.h"
 #include "math.h"
 
+typedef struct
+{
+  int type;
+  long num;
+  int err;
+} lval;
+
+enum
+{
+  LVAL_NUM,
+  LVAL_ERR
+};
+
+enum
+{
+  LERR_DIV_ZERO,
+  LERR_BAD_OP,
+  LERR_BAD_NUM
+};
+
+lval lval_num(long x)
+{
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+lval lval_err(int x)
+{
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+void lval_print(lval v)
+{
+  switch (v.type)
+  {
+  case LVAL_NUM:
+    printf("%li", v.num);
+    break;
+  case LVAL_ERR:
+    if (v.err == LERR_DIV_ZERO)
+    {
+      printf("Error: Divided by zero");
+    }
+    if (v.err == LERR_BAD_OP)
+    {
+      printf("Error: Invalid operator");
+    }
+    if (v.err == LERR_BAD_NUM)
+    {
+      printf("Error: Invalid number");
+    }
+    break;
+  }
+}
+
+void lval_println(lval v)
+{
+  lval_print(v);
+  putchar('\n');
+}
+
 long eval_op(char *op, long x, long y)
 {
   if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0)
